eae0b48cbca9e37518d3b84dda1271614a7565ff
Wong Fu Jie <Sable_021@yahoo.com.sg>
2023-12-12 17:47:22 +0800

    Added GroupEdit page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import Home from "./Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import GroupEdit from "./GroupEdit";
 import GroupList from "./GroupList";
-// import GroupEdit from "./GroupEdit";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/groups" exact={true} element={<GroupList />} />
+        <Route path="/groups/:id" element={<GroupEdit />} />
       </Routes>
     </Router>
   );
